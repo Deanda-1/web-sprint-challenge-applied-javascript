e58ad3ceff818c46692932939e86da1eb442615a
DJ1 <jimmynoble604@gmail.com>
2023-02-01 23:20:07 -0500

    Task 1 updated

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,21 @@
 const Header = (title, date, temp) => {
-  const header = document.createElement("div");
-  const dateElem = document.createElement("span");
-  const titleElem = document.createElement("h1");
-  const tempElem = document.createElement("span");
-  header.classList.add("header");
-  dateElem.classList.add("date");
-  tempElem.classList.add("temp");
+  const header = document.createElement('div');
+  const dateElem = document.createElement('span');
+  const titleElem = document.createElement('h1');
+  const tempElem = document.createElement('span');
+
+  header.classList.add('header');
+  dateElem.classList.add('date');
+  tempElem.classList.add('temp');
+
   header.appendChild(dateElem);
   header.appendChild(titleElem);
   header.appendChild(tempElem);
-  dateElem.textContent = "date";
-  titleElem.textContent = "title";
-  tempElem.textContent = "temp";
+
+  dateElem.textContent = date;
+  titleElem.textContent = title;
+  tempElem.textContent = temp;
+
   return header
   // TASK 1
   // ---------------------
